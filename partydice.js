431234188d4f7df795d31a56fb81292e5caee6a7
legandtime <legandtime@gmail.com>
2024-04-26 10:53:12 -0400

    Update partydice.js

--- partydice.js
+++ partydice.js
@@ -3,49 +3,49 @@ Hooks.once('diceSoNiceReady', (dice3d) => {
 	dice3d.addDicePreset({
 		type: "d2",
 		labels: "",
-		modelFile: "modules/Party Dice/PartyDice_d2.glb",
+		modelFile: "modules/PartyDice/PartyDice_d2.glb",
 		system: "PartyDice"
 	});
 	dice3d.addDicePreset({
 		type: "d4",
 		labels: "",
-		modelFile: "modules/Party Dice/PartyDice_d4.glb",
+		modelFile: "modules/PartyDice/PartyDice_d4.glb",
 		system: "PartyDice"
 	});
 	dice3d.addDicePreset({
 		type: "d6",
 		labels: "",
-		modelFile: "modules/Party Dice/PartyDice_d6.glb",
+		modelFile: "modules/PartyDice/PartyDice_d6.glb",
 		system: "PartyDice"
 	});
 	dice3d.addDicePreset({
 		type: "d8",
 		labels: "",
-		modelFile: "modules/Party Dice/PartyDice_d8.glb",
+		modelFile: "modules/PartyDice/PartyDice_d8.glb",
 		system: "PartyDice"
 	});
 	dice3d.addDicePreset({
 		type: "d10",
 		labels: "",
-		modelFile: "modules/Party Dice/PartyDice_d10.glb",
+		modelFile: "modules/PartyDice/PartyDice_d10.glb",
 		system: "PartyDice"
 	});
 	dice3d.addDicePreset({
 		type: "d100",
 		labels: "",
-		modelFile: "modules/Party Dice/PartyDice_d100.glb",
+		modelFile: "modules/PartyDice/PartyDice_d100.glb",
 		system: "PartyDice"
 	});
 	dice3d.addDicePreset({
 		type: "d12",
 		labels: "",
-		modelFile: "modules/Party Dice/PartyDice_d12.glb",
+		modelFile: "modules/PartyDice/PartyDice_d12.glb",
 		system: "PartyDice"
 	});	
 	dice3d.addDicePreset({
 		type: "d20",
 		labels: "",
-		modelFile: "modules/Party Dice/PartyDice_20.glb",
+		modelFile: "modules/PartyDice/PartyDice_20.glb",
 		system: "PartyDice"
 	});
 });
